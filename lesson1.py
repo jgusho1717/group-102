55c57c35b0c00228aced854127a94ba538387861
jgusho1717 <rezijgushia17@gmail.com>
2024-02-05 04:50:32 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,36 @@
+name = " rezi"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "rezi"  არის ცვლადისთვის მნიშვნელობა
+surname = " and my surname is jgushia"
+
+#print(name)
+
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "my name is rezi" #ეს არის str (string) ტიპის ცვლადი
+
+age = 'i am 15 years old' # ეს არის int (integer) მთელი რიცხვი 
+height = 'my height is 170' #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True  #true or false
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+is_Ugly = False # ჯავასკრიპტული camelcase
+
+print(name + "  " + surname)
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(knows_programming))
+
+
+#print(name + age)
+
+print(name + " " + str(age))
+print(name + " " + str(height))
+
+print(name + " " + surname + " " + (str(age))) 
+
